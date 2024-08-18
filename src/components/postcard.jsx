f283b017eae92384c19da66d5b4c427c718663b9
Alok Kumar <122669937+alokthakur490@users.noreply.github.com>
2024-08-18 16:12:33 +0530

    read more button

--- src/components/postcard.jsx
+++ src/components/postcard.jsx
@@ -20,15 +20,15 @@ function CustomCard() {
           The place is close to Barceloneta Beach and bus stop just 2 min by walk and near to "Naviglio" where you can enjoy the main nightlife in Barcelona.
         </p>
       </div>
-      <div className="p-6 pt-0">
-        <button
-          className=""
-          type="button"
-        >
-          Read More
-        </button>
-      </div>
+      <div class="p-6 pt-0">
+      <button
+      className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-blue text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
+      type="button"
+    >
+      Read More
+    </button>
     </div>
+      </div>  
   );
 }
 
