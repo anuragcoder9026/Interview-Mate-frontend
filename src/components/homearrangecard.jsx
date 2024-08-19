106628bb8d293acc9db309268f8af8805163c767
Alok Kumar <122669937+alokthakur490@users.noreply.github.com>
2024-08-20 00:34:07 +0530

    blogs added

--- src/components/homearrangecard.jsx
+++ src/components/homearrangecard.jsx
@@ -3,14 +3,23 @@ import CustomCard from "./postcard"; // Adjust the path as necessary
 
 function CardGrid() {
   return (
-    <div className="relative flex flex-wrap justify-center gap-6 p-2 sm:p-6 bg-slate-200">
-      <CustomCard />
-      <CustomCard />
-      <CustomCard />
-      <CustomCard />
-      <CustomCard />
-      <CustomCard />
-      {/* Add more CustomCard components as needed */}
+    <div className="bg-slate-200 p-2 sm:p-8">
+      <div className="relative flex items-center pt-4">
+        <h1 className="pl-2 sm:pl-4 md:pl-6 lg:pl-8 text-3xl sm:text-4xl font-serif font-semibold text-gray-800 mr-4">
+          Blogs
+        </h1>
+
+        <hr className="border-t-2 border-gray-300 w-full sm:w-3/3" />
+      </div>
+      <div className="relative flex flex-wrap justify-center gap-6 pt-6 pb-6">
+        <CustomCard />
+        <CustomCard />
+        <CustomCard />
+        <CustomCard />
+        <CustomCard />
+        <CustomCard />
+        {/* Add more CustomCard components as needed */}
+      </div>
     </div>
   );
 }
