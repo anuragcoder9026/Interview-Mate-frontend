6558cead2f3a1a575e93951a5a589a01eabb54ab
Anurag Singh <iitpatelanurag2003@gmail.com>
2024-08-19 06:08:21 +0530

    dashboard section added

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,15 @@ import { FooterWithSocialLinks } from './components/footer'
 import About from './components/about'
 import Card from './components/card'
 import CardGrid from './components/homearrangecard'
-
-
 import './App.css'
-
+import { Outlet } from "react-router-dom";
 function App() {
 
   return (
     <>
      <Header/>
      <NavBar/>
-     <CardGrid/>
-     {/* <Card/> */}
-     
-     {/* <About/> */}
+     <Outlet/>
      <FooterWithSocialLinks/>
     </>
   )
