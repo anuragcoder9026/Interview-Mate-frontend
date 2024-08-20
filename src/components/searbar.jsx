3a14989b5c705a79a06dac00b5dffda2a7a5f3f7
Alok Kumar <122669937+alokthakur490@users.noreply.github.com>
2024-08-21 01:10:40 +0530

    Update searbar.jsx

--- src/components/searbar.jsx
+++ src/components/searbar.jsx
@@ -14,7 +14,6 @@ const SearchBar = () => {
         if (searchTerm.trim()) {
             const url = `https://www.google.com/search?q=${encodeURIComponent(searchTerm)}`;
             window.location.href = url;
-            setIsExpanded(false); // Shrink after taking input
         }
     };
 
