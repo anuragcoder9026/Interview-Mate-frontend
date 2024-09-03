bcae412b09ccbcfbbd84e0f906ba27da6a825d05
Alok Kumar <122669937+alokthakur490@users.noreply.github.com>
2024-09-03 23:10:41 +0530

    css added

--- src/components/QuizApp/edit.jsx
+++ src/components/QuizApp/edit.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import { Editor } from '@monaco-editor/react';
-import PropTypes from 'prop-types'; // Import PropTypes for validation
-
+import './editcss.css'; // Import external CSS file for responsive styling
 const CodeEditor = ({ codeString }) => {
   const formattedCode = codeString;
+//   preprocessCode(codeString);
 
   return (
-    <div className="w-full max-h-[300px] rounded-lg mb-2.5
-                    sm:h-[300px] md:h-[400px] lg:h-[500px]">
+    <div className="code-editor-container">
       <Editor
-        height="100%" // Use 100% to fit the container height
+        height="92%"
         language="cpp"
         value={formattedCode}
         theme="vs-dark"
@@ -22,11 +21,6 @@ const CodeEditor = ({ codeString }) => {
       />
     </div>
   );
-};
-
-// Define prop types for better validation
-CodeEditor.propTypes = {
-  codeString: PropTypes.string.isRequired,
 };
 
 export default CodeEditor;
