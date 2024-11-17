bf5a14d0244c318ad7d7ab31bdf608222313f6b3
Alok Kumar <122669937+alokthakur490@users.noreply.github.com>
2024-11-17 16:25:38 +0530

    Update url.js

--- url.js
+++ url.js
@@ -3,4 +3,5 @@
 // export const URL='http://localhost:3200';
 //export const BACKEND_URL='http://localhost:3200'
 //export const BACKEND_URL='https://interview-mate-backend-41xo.onrender.com'
+
 export const BACKEND_URL='https://interview-mate-backend.onrender.com'
